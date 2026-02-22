af54b42aeaa17657297dfc5de1ba501df977915b
NIHAL  T P <nihaltpnki@gmail.com>
2026-02-22 14:29:57 +0530

    fix: Correct delegate assignment for UNUserNotificationCenter in AppDelegate

--- mobile_app/ios/Runner/AppDelegate.swift
+++ mobile_app/ios/Runner/AppDelegate.swift
@@ -14,7 +14,7 @@ import flutter_local_notifications
     }
     GeneratedPluginRegistrant.register(with: self)
     if #available(iOS 10.0, *) {
-    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    UNUserNotificationCenter.current().delegate = self
     }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
